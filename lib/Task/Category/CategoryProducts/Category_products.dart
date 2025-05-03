bcbb14eedd9a5918bfadfd43c027e13df7327095
Rajan <krajan92946@gmail.com>
2025-05-03 19:00:30 +0530

    added category dynamic and navigate to category page

---
+++ lib/Task/Category/CategoryProducts/Category_products.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+class CategoryProducts extends StatelessWidget {
+  const CategoryProducts({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar:  AppBar(title: Text("Category Product"),),
+    );
+  }
+}
